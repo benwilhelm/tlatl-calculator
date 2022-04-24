5988a02e7ea6ed2c2f44c7e37f0b383930ecbf91
Ben Wilhelm <benjamin.m.wilhelm@gmail.com>
2022-04-24 16:26:24 -0500

    flesh out domain test

--- app/domain.spec.js
+++ app/domain.spec.js
@@ -2,8 +2,15 @@ import { evaluateTokens, InvalidExpressionError } from './domain.js';
 
 describe('evaluateTokens', () => {
   test('should evaluate simple sequences of + - * /', () => {
+    // Check all four operators
     expect(evaluateTokens([1, '+', 3])).toEqual(4);
+    expect(evaluateTokens([7, '-', 1])).toEqual(6);
+    expect(evaluateTokens([14, '/', 2])).toEqual(7);
+    expect(evaluateTokens([7, '*', 3])).toEqual(21);
+
+    // sequences can include multiple operators
     expect(evaluateTokens([1, '+', 3, '-', 5.5])).toEqual(-1.5);
+    expect(evaluateTokens([7, '*', 3, '-', 10])).toEqual(11);
   });
 
   test('should throw for invalid sequences', () => {
